3d7b1047f6203ddb3f6bb26bbfd14733fdfaef04
Bhavishya2011 <132471638+Bhavishya2011@users.noreply.github.com>
2024-01-31 18:36:40 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -7,11 +7,46 @@ function setup() {
 }
 
 function draw() {
-    image(video, 0, 0, 600, 500);
+	image(video, 0, 0, 600, 500);
+	
+	song1_status = song1.isPlaying();
+	song2_status = song2.isPlaying();
+
+	fill("#FF0000");
+	stroke("#FF0000");
+
+	if(scoreRightWrist > 0.2)
+	{ 
+		circle(rightWristX,rightWristY,20);
+
+			song2.stop();
+
+		if(song1_status == false)
+		{
+			song1.play();
+			document.getElementById("song").innerHTML = "Playing - Charmer"
+		}
+	}
+
+	if(scoreLeftWrist > 0.2)
+	{
+		circle(leftWristX,leftWristY,20);
+
+			song1.stop();
+
+		if(song2_status == false)
+		{
+			song2.play();
+			document.getElementById("song").innerHTML = "Playing - My Bag"
+		}
+	}
+
 }
 
 song1 = "";
 song2 = "";
+song1_status = "";
+song2_status = "";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
